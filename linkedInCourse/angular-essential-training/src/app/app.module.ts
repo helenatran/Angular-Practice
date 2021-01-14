162345c1a8914f1ae624464dd3357a2f54d08239
helenatran <helena.e.tran@student.uts.edu.au>
2021-01-14 14:48:08 +1100

    Create new component and work on directives/pipes

--- linkedInCourse/angular-essential-training/src/app/app.module.ts
+++ linkedInCourse/angular-essential-training/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item/media-item.component';
+import { MediaItemListComponent } from './media-item-list/media-item-list.component';
+import { FavoriteDirective } from './favorite.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaItemComponent,
+    MediaItemListComponent,
+    FavoriteDirective
   ],
   imports: [
     BrowserModule,
